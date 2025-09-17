8c54b1628128795122b00306d1cb7ab97bb0ea02
Miles Hilton <miles.p.hilton@gmail.com>
2025-09-17 14:47:49 -0400

    resources page

---
+++ content/resources.js
@@ -0,0 +1,19 @@
+lesbianWomenContent = [
+    "<u>Lesbian Emergency Fund</u> <i>Email Chloe</i> regarding small grants to local lesbians/queer women in any kind of crises.",
+    "two"
+];
+
+queerContent = [
+    "Queer Humboldt individual therapy, support groups, community events, and more including a hate event reporting portal.",
+    "two"
+];
+
+helplinesContent = [
+    "Raven Youth Services for ages 16­25. Services include housing assistance. 24hr hotline 707­444­2273.",
+    "two"
+];
+
+moreContent = [
+    "Centro de Pueblo support for people who are indigenous, Latinx, and/or immigrants. Sanctuary Garden 11th & F, Arcata. ICE hotline 707­200.8091.",
+    "two"
+];
