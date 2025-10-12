e10d62fceaf66c71a49dae059f670ce218d17030
redwoodviolet <afrasche@proton.me>
2025-10-12 16:27:36 -0700

    Update resources.js

--- content/resources.js
+++ content/resources.js
@@ -1,11 +1,10 @@
 lesbianWomenContent = [
     "<u>Lesbian Emergency Fund</u> <i>Email Chloe</i> regarding small grants to local lesbians/queer women in any kind of crises.",
-    "two"
-    "LWord Digital Archive back issues (currently 1988 89) digitalcommons.humboldt.edu/l word"
-    "Lesbian Connection"
-    "CPH Women’s Resource Center 826 4216 menstrual & safer sex supplies; events."
-    "Breast & GYN Health Project Help finding preventative breast health & navigating breast or gynecologic cancers. 825 8345"
-    "Planned Parenthood Eureka preventative, gynecological, reproductive, and gender affirming care; medical & surgical abortions. 442 5700."
+    "LWord Digital Archive back issues (currently 1988 89) digitalcommons.humboldt.edu/l word",
+    "Lesbian Connection",
+    "CPH Women’s Resource Center 826 4216 menstrual & safer sex supplies; events.",
+    "Breast & GYN Health Project Help finding preventative breast health & navigating breast or gynecologic cancers. 825 8345",
+    "Planned Parenthood Eureka preventative, gynecological, reproductive, and gender affirming care; medical & surgical abortions. 442 5700.",
     "Arcata Community Health Center preventative, gynecological, & gender affirming care. 826 8610"
 
 
