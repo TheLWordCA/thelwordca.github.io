d1f34427e737f7f41996644daa478343285f8941
redwoodviolet <afrasche@proton.me>
2025-10-13 13:31:57 -0700

    Update resources.js
    added ]; to the end

--- content/resources.js
+++ content/resources.js
@@ -6,7 +6,7 @@ lesbianWomenContent = [
     "Breast & GYN Health Project Help finding preventative breast health & navigating breast or gynecologic cancers. 825 8345",
     "Planned Parenthood Eureka preventative, gynecological, reproductive, and gender affirming care; medical & surgical abortions. 442 5700.",
     "Arcata Community Health Center preventative, gynecological, & gender affirming care. 826 8610"
-
+];
 
 queerContent = [
     "Queer Humboldt individual therapy, support groups, community events, and more including a hate event reporting portal.",
