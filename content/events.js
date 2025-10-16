428e278e4fa3a177f20c8c0a1d194166b128c43a
lwghsue <suejh@humboldt1.com>
2025-10-15 19:19:05 -0700

    Update event details for October 18 rally

--- content/events.js
+++ content/events.js
@@ -1,8 +1,8 @@
 events = {
     "events": [
         {
-            "title": "test",
-            "body": "hi"
+            "title": "No Kings II October 18",
+            "body": "Join people around the country in resisting vote-stealing, masked gunmen in our streets, acceleration of the climate crisis, lying, lawbreaking, and attacks on just about everyone so the rich can get richer.  Saturday October 18, starting at 11 am at the Eureka co-op and walking on the sidewalks in groups to the 4th street side of the courthouse.  On the 5th street side, the rally goes from noon-2pm.  Bring signs and energy."
         },
         {
             "title": "hello",
