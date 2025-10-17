9fc3f1a3fe242d9a04fcce6d6db4759bacf39267
lwghsue <suejh@humboldt1.com>
2025-10-17 10:04:24 -0700

    Add Dia de los Muertos event details

--- content/events.js
+++ content/events.js
@@ -8,5 +8,10 @@ events = {
             "title": "Celebrate Queer History",
             "body": "Queer Humboldt, the Eric Rofes center at Cal Poly Humboldt and Equity Arcata are inviting us to a Queer History Party. Lots of community groups with activities, a costume contest for Favorite Queer Person from History, a drag show, and food and drink trucks.  The LWord will have coloring sheets of historic Pride drawings--to help table, email suejh@humboldt1.com. Sunday October 26, 4-7 pm, D street Neighborhood Center in Arcata. info Aisha@queerhumboldt.org."
         },
+        {
+            "title": "Dia de los Muertos",
+            "body": "Join Centro del Pueblo in Celebrating Dia de los Muertos. There's a masked parade in old town Eureka on October 30 starting at 6 pm, and a festival with Catrinas, dancing, alters, and more at the Eureka theater 4-6 pm on November 2, and lots of ways to be more involved.
+For the parade, there are mask workshops at Centro's Eureka office (3008 Broadway) Fridays Oct. 17 & 24, 6-7:30pm. For the La Catrina show, there are rehearsals through October, on Thursdays at the Eureka office 6-7:45pm and Saturdays at the Jardín Santuario in Arcata 9:30- 11am. Contact laura@cdpueblo.com to get involved. You can also make Sugar skulls at el Jardín Santuario 10/18 and 10/25,12pm. And celebrate the harvest at CdP's farm, October 17-19 starting at 10 am. <a href=\"https:\/\/cdpueblo.com/events/#coming-up-events">cdpueblo.com/events/#coming-up-events</a>"
+        },
     ]
 };
