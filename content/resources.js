107ce86a9a9087b76f99b278774f7161e871d6d6
redwoodviolet <afrasche@proton.me>
2025-10-28 16:54:50 -0700

    Update resources.js
    added dashes, link for "email Chloe"

--- content/resources.js
+++ content/resources.js
@@ -1,11 +1,11 @@
 lesbianWomenContent = [
-    "<u>Lesbian Emergency Fund</u> <i>Email Chloe</i> regarding small grants to local lesbians/queer women in any kind of crises.",
-    "LWord Digital Archive back issues (currently 1988 89) digitalcommons.humboldt.edu/l word",
+    "<u>Lesbian Emergency Fund</u> <a href="mailto:chloeqc@protonmail.com>email Chloe</a> regarding small grants to local lesbians/queer women in any kind of crises.",
+    "LWord Digital Archive back issues (currently 1988 - 91) digitalcommons.humboldt.edu/l word",
     "Lesbian Connection",
-    "CPH Women’s Resource Center 826 4216 menstrual & safer sex supplies; events.",
-    "Breast & GYN Health Project Help finding preventative breast health & navigating breast or gynecologic cancers. 825 8345",
-    "Planned Parenthood Eureka preventative, gynecological, reproductive, and gender affirming care; medical & surgical abortions. 442 5700.",
-    "Arcata Community Health Center preventative, gynecological, & gender affirming care. 826 8610"
+    "CPH Women’s Resource Center 826-4216 menstrual & safer sex supplies; events.",
+    "Breast & GYN Health Project Help finding preventative breast health & navigating breast or gynecologic cancers. 825-8345",
+    "Planned Parenthood Eureka preventative, gynecological, reproductive, and gender affirming care; medical & surgical abortions. 442-5700.",
+    "Arcata Community Health Center preventative, gynecological, & gender affirming care. 826-8610"
 ];
 
 queerContent = [
@@ -13,19 +13,19 @@ queerContent = [
     "Redwood Pride supporting and building LGBTQIA community. 1st & 3rd Sunday except January 12 - 2 pm Jefferson Center, 1000 B st. Eureka.",
     "Lost Coast Pride Pride Ride and Ferndale Pride march.",
     "Del Norte LGBTQ.org: resources and info, Pride & other events, weekly gatherings and more",
-    "College of the Redwoods Queer Students Union, weekly when school is in session. 476 4255",
+    "College of the Redwoods Queer Students Union, weekly when school is in session. 476-4255",
     "Cal Poly Humboldt: Queer Students Union, QWEERS (queer staff), and Eric Rofes Center for Multicultural Queer Studies NH east rm 102",
     "Eureka Sisters of Perpetual Indulgence. Meetings 1st Sat. 11 am 1000 B Eureka",
-    "Northcoast AIDS project Preventative care for all; comprehensive services for people with HIV or AIDS. 268 2132."
+    "Northcoast AIDS project Preventative care for all; comprehensive services for people with HIV or AIDS. 268-2132."
 ];
 
 helplinesContent = [
-    "Raven Youth Services for ages 16­25. Services include housing assistance. 24hr hotline 707­444­2273.",
+    "Raven Youth Services for ages 16-­25. Services include housing assistance. 24hr hotline 707-­444-­2273.",
     "two",
     "this is a test"
 ];
 
 moreContent = [
-    "Centro de Pueblo support for people who are indigenous, Latinx, and/or immigrants. Sanctuary Garden 11th & F, Arcata. ICE hotline 707­200.8091.",
+    "Centro de Pueblo support for people who are indigenous, Latinx, and/or immigrants. Sanctuary Garden 11th & F, Arcata. ICE hotline 707­-200-8091.",
     "two"
 ];
