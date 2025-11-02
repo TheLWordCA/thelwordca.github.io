a0eef982ba69fd1d43e4aaeb8a07a29728ea2afd
lwghsue <suejh@humboldt1.com>
2025-11-01 21:07:41 -0700

    Add Trans Remembrance and Resistance event details

--- content/events.js
+++ content/events.js
@@ -12,5 +12,9 @@ events = {
             "title": "Caminata procession",
             "body": "Join Centro del Pueblo in walking from Fortuna to Eureka, advocating for the release of people in ICE detention, on Saturday November 8. cdpueblo.com"
         },
+        {
+            "title": "Trans Remembrance and Resistance",
+            "body": "There are events in Del Norte and around Humboldt Bay to remember Trans* lives lost to violence and to celebrate and support out communites.  In Del Norte, the DNATL LGBTQ Project is hosting a Transgender Day of Remembrance & Resilience on Nov. 20th starting at 6pm. St Paul's Episcopal, 220 E Macken St. Crescent City. Around Humboldt Bay, The Eureka Sisters are hosting a week of events, including speed friending, a talking circle, film festival, art & poetry, the TRANScending the Veil ritual on the Arcata Plaza at 5 pm on Thursday Nov. 20, a music and drag show, and more. eurekasister.org"
+        },
     ]
 };
