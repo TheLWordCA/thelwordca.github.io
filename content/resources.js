19b8803204d1035b47c9cda78d609c54230de006
lwghsue <suejh@humboldt1.com>
2025-11-04 11:52:54 -0800

    Update content formatting and links in resources.js

--- content/resources.js
+++ content/resources.js
@@ -1,28 +1,28 @@
 lesbianWomenContent = [
-    "<u>Lesbian Emergency Fund</u><a href="mailto:chloeqc@protonmail.com>email Chloe</a> regarding small grants to local lesbians/queer women in any kind of crises.",
-    "LWord Digital Archive back issues (currently 1988 - 91) digitalcommons.humboldt.edu/l word",
-    "Lesbian Connection",
+    'Lesbian Emergency Fund <a href="mailto:chloeqc@protonmail.com">email Chloe</a> regarding small grants to local lesbians/queer women in any kind of crises.',
+    'LWord Digital Archive back issues (currently 1988 - 91) <a href="https://digitalcommons.humboldt.edu/l-word/">lword archive at Cal Poly Humboldt</a>',
+    'Lesbian Connection - free worldwide forum of news & ideas for by and about lesbians <a href="https://www.lconline.org/">lconline.org</a>',
     "CPH Women’s Resource Center 826-4216 menstrual & safer sex supplies; events.",
-    "Breast & GYN Health Project Help finding preventative breast health & navigating breast or gynecologic cancers. 825-8345",
-    "Planned Parenthood Eureka preventative, gynecological, reproductive, and gender affirming care; medical & surgical abortions. 442-5700.",
-    "Arcata Community Health Center preventative, gynecological, & gender affirming care. 826-8610"
+    "Breast & GYN Health Project Help finding preventative breast health & navigating breast or gynecologic cancers. 707 825-8345",
+    "Planned Parenthood Eureka preventative, gynecological, reproductive, and gender affirming care; medical & surgical abortions. 707 442-5700.",
+    "Arcata Community Health Center preventative, gynecological, & gender affirming care. 707 826-8610",
 ];
 
 queerContent = [
-    "Queer Humboldt individual therapy, support groups, community events, and more including a hate event reporting portal.",
-    "Redwood Pride supporting and building LGBTQIA community. 1st & 3rd Sunday except January 12 - 2 pm Jefferson Center, 1000 B st. Eureka.",
+    'Queer Humboldt individual therapy, support groups, community events, and more including a hate event reporting portal.<a href="https://www.queerhumboldt.org/">queerhumboldt.org</a>',
+    "Redwood Pride supporting and building LGBTQIA community. 1st & 3rd Sunday except January. 12 - 2 pm Jefferson Center, 1000 B st. Eureka.",
     "Lost Coast Pride Pride Ride and Ferndale Pride march.",
-    "Del Norte LGBTQ.org: resources and info, Pride & other events, weekly gatherings and more",
-    "College of the Redwoods Queer Students Union, weekly when school is in session. 476-4255",
-    "Cal Poly Humboldt: Queer Students Union, QWEERS (queer staff), and Eric Rofes Center for Multicultural Queer Studies NH east rm 102",
-    "Eureka Sisters of Perpetual Indulgence. Meetings 1st Sat. 11 am 1000 B Eureka",
-    "Northcoast AIDS project Preventative care for all; comprehensive services for people with HIV or AIDS. 268-2132."
+    'Del Norte LGBTQ.org: resources and info. Pride & other events. weekly gatherings and more <a href="https://www.dnlgbtq.org/">DNLGBTQ.org</a>',
+    "College of the Redwoods Queer Students Union, weekly when school is in session. 707 476-4255",
+    "Cal Poly Humboldt: Queer Students Union. QWEERS (queer staff). and Eric Rofes Center for Multicultural Queer Studies NH east rm 102",
+    'Eureka Sisters of Perpetual Indulgence. Meetings 1st Sat. 11am 1000 B Eureka. <a href="https://www.eurekasisters.org/">eurekasisters.org</a>',
+    "Northcoast AIDS project Preventative care for all. Comprehensive services for people with HIV or AIDS. 707 268-2132.",
 ];
 
 helplinesContent = [
-    "Raven Youth Services for ages 16-­25. Services include housing assistance. 24hr hotline 707-­444-­2273."]
-    "two",
-    "this is a test"
+    "Raven Youth Services for ages 16-­25. Services include housing assistance. 24hr hotline 707-­444-­2273.",
+    "North Coast Rape Crisis Team 24 hour sexual assault crisis line 707 445 2882. Also support groups and education",
+    "Humboldt Domestic Violence Services support groups and 24 hr support line 707 443 6042",
 ];
 
 moreContent = [
