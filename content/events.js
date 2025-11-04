c98718cffa87954ed38f71e50a21c23db612893a
lwghsue <suejh@humboldt1.com>
2025-11-04 11:03:33 -0800

    Fix formatting and links in event description
    Updated the body text for the 'Trans Remembrance and Resistance' event to correct formatting and links.

--- content/events.js
+++ content/events.js
@@ -10,7 +10,7 @@ events = {
         },
         {
             "title": "Trans Remembrance and Resistance",
-            "body": "There are events in Del Norte and around Humboldt Bay to remember Trans* lives lost to violence and to celebrate and support out communites.  In Del Norte, the DNATL LGBTQ Project is hosting a Transgender Day of Remembrance & Resilience on Nov. 20th starting at 6pm. St Paul's Episcopal, 220 E Macken St. Crescent City. Around Humboldt Bay, The Eureka Sisters are hosting a week of events, including speed friending, a talking circle, film festival, art & poetry, the TRANScending the Veil ritual on the Arcata Plaza at 5 pm on Thursday Nov. 20, a music and drag show, and more. eurekasister.org"
+            "body": 'There are events in Del Norte and around Humboldt Bay to remember Trans* lives lost to violence and to celebrate and support out communites.  In Del Norte, the DNATL LGBTQ Project is hosting a Transgender Day of Remembrance & Resilience on Nov. 20th starting at 6pm. St Pauls Episcopal, 220 E Macken St. Crescent City. Around Humboldt Bay, The Eureka Sisters are hosting a week of events, including speed friending, a talking circle, film festival, art & poetry, the TRANScending the Veil ritual on the Arcata Plaza at 5 pm on Thursday Nov. 20, a music and drag show, and more. <ahref="http//eurekasisters.org>eurekasisters.org</a>'"
         },
     ]
 };
