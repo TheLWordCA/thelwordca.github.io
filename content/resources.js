fd44bf7875ae3eb317e0fcc3893ba054cc216786
redwoodviolet <afrasche@proton.me>
2025-11-06 13:34:00 -0800

    Update resources.js

--- content/resources.js
+++ content/resources.js
@@ -1,7 +1,7 @@
 lesbianWomenContent = [
     'Lesbian Emergency Fund <a href="mailto:chloeqc@protonmail.com">email Chloe</a> regarding small grants to local lesbians/queer women in any kind of crises.',
     '<a href="https://digitalcommons.humboldt.edu/l-word/">LWord Digital Archive back issues</a> (currently 1988 - 93)',
-    '<a href="https://www.lconline.org/">Lesbian Connection"</a>- free worldwide forum of news & ideas for by and about lesbians',
+    '<a href="https://www.lconline.org/">Lesbian Connection</a>- free worldwide forum of news & ideas for by and about lesbians',
     '<a href="https://www.humboldt.edu/womens-resource-center">CPH Women’s Resource Center</a> 826-4216 menstrual & safer sex supplies; events.',
     '<a href="https://bghp.org/">Breast & GYN Health Project Help</a> finding preventative breast health & navigating breast or gynecologic cancers. 707 825-8345',
     '<a href="https://www.plannedparenthood.org/health-center/california/eureka/95503/eureka-health-center-2301-90200">Planned Parenthood Eureka</a> preventative, gynecological, reproductive, and gender affirming care; medical & surgical abortions. 707 442-5700.',
@@ -9,14 +9,14 @@ lesbianWomenContent = [
 ];
 
 queerContent = [
-    'Queer Humboldt individual therapy, support groups, community events, and more including a hate event reporting portal.<a href="https://www.queerhumboldt.org/">queerhumboldt.org</a>',
-    "Redwood Pride supporting and building LGBTQIA community. 1st & 3rd Sunday except January. 12 - 2 pm Jefferson Center, 1000 B st. Eureka.",
-    "Lost Coast Pride Pride Ride and Ferndale Pride march.",
-    'Del Norte LGBTQ.org: resources and info. Pride & other events. weekly gatherings and more <a href="https://www.dnlgbtq.org/">DNLGBTQ.org</a>',
-    "College of the Redwoods Queer Students Union, weekly when school is in session. 707 476-4255",
-    "Cal Poly Humboldt: Queer Students Union. QWEERS (queer staff). and Eric Rofes Center for Multicultural Queer Studies NH east rm 102",
-    'Eureka Sisters of Perpetual Indulgence. Meetings 1st Sat. 11am 1000 B Eureka. <a href="https://www.eurekasisters.org/">eurekasisters.org</a>',
-    "Northcoast AIDS project Preventative care for all. Comprehensive services for people with HIV or AIDS. 707 268-2132.",
+    '<a href="https://www.queerhumboldt.org/">Queer Humboldt</a> individual therapy, support groups, community events, and more including a <a href=https://www.queerhumboldt.org/hate-event-reporting-portal>hate event reporting portal</a>.
+    '<a href="https://www.inkpeople.org/dreammaker-data/redwoodpride">Redwood Pride</a> supporting and building LGBTQIA community. 1st & 3rd Sunday except January. 12 - 2 pm Jefferson Center, 1000 B st. Eureka.',
+    '<a href="mailto:HumboldtsPride@gmail.com">Lost Coast Pride</a> Pride Ride and Ferndale Pride march.',
+    '<a href="https://www.dnlgbtq.org/">Del Norte LGBTQ Resources Project</a> resources and info. Pride & other events. weekly gatherings and more',
+    'College of the Redwoods Queer Students Union, weekly when school is in session. 707 476-4255',
+    'Cal Poly Humboldt<a href="mailto:qsu@humboldt.edu">Queer Students Union</a>, <a href=https://www.humboldt.edu/qweerss>QWEERSS</a> (queer staff), and <a href="https://www.humboldt.edu/erc">Eric Rofes Center</a>',
+    '<a href="https://www.eurekasisters.org/">Eureka Sisters</a>of Perpetual Indulgence. Meetings 1st Sat. 11am 1000 B Eureka,
+    '<a href="https://humboldtgov.org/620/North-Coast-AIDS-Project-NorCAP">Northcoast AIDS project</a> Preventative care for all. Comprehensive services for people with HIV or AIDS. 707 268-2132.',
 ];
 
 helplinesContent = [
