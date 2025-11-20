0d1fe07140f7985beccf5554f31e26efd96900a8
lwghsue <suejh@humboldt1.com>
2025-11-20 09:58:11 -0800

    Update events list with new entries and remove one
    Removed Caminata procession event and added two new events: Hands Off Venezuela and Andrea Gibson at the Summit.

--- content/events.js
+++ content/events.js
@@ -1,12 +1,16 @@
 events = {
     "events": [
-         {
-            "title": "Caminata procession",
-            "body": 'Join Centro del Pueblo in walking from Fortuna to Eureka, advocating for the release of people in ICE detention, on Saturday November 8. <a href="http://cdpueblo.com/">cdpueblo.com</a>'
-        },
         {
             "title": "Trans Remembrance and Resistance",
             "body": 'There are events in Del Norte and around Humboldt Bay to remember Trans* lives lost to violence and to celebrate and support our communites.  In Del Norte, the DNATL LGBTQ Project is hosting a Transgender Day of Remembrance & Resilience on Nov. 20th starting at 6pm. St Pauls Episcopal, 220 E Macken St. Crescent City. Around Humboldt Bay, The Eureka Sisters are hosting a week of events, including speed friending, a talking circle, film festival, art & poetry, the TRANScending the Veil ritual on the Arcata Plaza at 5 pm on Thursday Nov. 20, a music and drag show, and more. <a href="http//eurekasisters.org">eurekasisters.org</a>'
+        },
+        {   
+            "title": "Hands Off Venezuela",
+         "body": "Speak out against our government's random killings and threats against Venezualan people at the Humboldt County Courthouse, 4:30-6 pm Thursday Nov. 20."
+        }
+        { 
+            "title": "Andrea Gibson at the Summitt",
+            "body": 'The Summitt galley & womens sports lounge is showing "Come see Me in the Good Light". The documentary about poet Andrea Gibson, at 6 pm, will be followed by an open mic to share her poetry at 8 pm.  Rooftop, 1521 G st. Arcata. 21+.'
         },
     ]
 };
