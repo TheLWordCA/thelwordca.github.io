a3c1d0e66206a01d4bfa3b4acc5c0b6623dbaaba
lwghsue <suejh@humboldt1.com>
2025-12-02 10:06:08 -0800

    Update events in events.js
    Removed events related to 'Hands Off Venezuela' and 'Andrea Gibson at the Summitt'. Added new event 'Gather Hold Ground'.

--- content/events.js
+++ content/events.js
@@ -5,12 +5,8 @@ events = {
             "body": 'World AIDS Day raises awareness, supports the fight against the disease, and remembers people who died.<b> In Del Norte</b>, the DNATL LGBTQ Project and the Del Norte County library are showing "Long Time Companion" about the early days of the epidemic on Dec. 4 4-6 pm at 190 Price Mall, Crescent City.  At <b>Cal Poly Humboldt</b>, a square of the AIDS memorial quilt will be at the library from Dec. 1-13. There will also be a zoom discussion about Asian-American AIDS activism Dec. 5, 5-6 pm in the library fishbowl, and a screening of Wilhelminas War, about a southern grandmother fighting to support her loved ones with HIV, on Dec. 10, 5:30-7:30 in the Fishbowl. And the <b>Eureka Sisters of Perpetual Indulgence</b> will be doing a Community Art Project 10-2 and a Ritual at noon on the Arcata Plaza on Dec 6. The North Coast AIDS project "https://humboldtgov.org/620/North-Coast-AIDS-Project-NorCAP/">NorCAP</a> provides ongoing local resources for HIV prevention and support for people living with it.'
         },
         {   
-            "title": "Hands Off Venezuela",
-         "body": "Speak out against our government's random killings and threats against Venezualan people at the Humboldt County Courthouse, 4:30-6 pm Thursday Nov. 20."
-        },
-        { 
-            "title": "Andrea Gibson at the Summitt",
-            "body": 'The Summitt galley & womens sports lounge is showing "Come see Me in the Good Light". The documentary about poet Andrea Gibson, at 6 pm, will be followed by an open mic to share her poetry at 8 pm.  Rooftop, 1521 G st. Arcata. 21+.'
+            "title": "Gather Hold Ground",
+         "body": "Gather Hold Ground brings together works carrying a central message of solidarity, memory, and community connection by Lilia Hernandez Galusha, including a zine library and hooked textiles. Benefits Centro del Pueblo. At the Reese Bullen Gallery, first floor of Art B at Cal Poly Humboldt through Dec. 13."
         }
     ]
 };
