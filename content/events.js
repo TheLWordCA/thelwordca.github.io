9ca2c528ad16909daf9b2062e26f411840ffd362
lwghsue <suejh@humboldt1.com>
2026-01-31 20:19:34 -0800

    Update event titles and descriptions

--- content/events.js
+++ content/events.js
@@ -1,20 +1,16 @@
 events = {
     "events": [
         {
-            "title": "Honor MLK Day",
-            "body": 'The Eureka branch of the NAACP invites you to a community clebration of Martin Luther King Day.  It starts at 1 pm on Monday Jan 19 at the Adorni Center 1011 Waterfront Drive in Eureka.  Music, information, food, and community. And Cal Poly Humboldt is inviting students, staff, and community to a half-day of service on Monday Jan. 19 <a href= "https://www.humboldt.edu/youth-educational-services/upcoming-events/mlk">humboldt.edu/youth-educational-services/upcoming-events/mlk</a>.'
+            "title": "Black Liberation Month",
+            "body": 'Celebrate Black Liberation month with Black Humboldt, Cal Poly Humboldt, and around the county.  Black Humboldt events include speakers at the Clarke Museum in Eureka on Saturday the 7th, 2:30-3:30pm, a Black History Month Timeline tour displayed at the Cal Poly Humboldt Library and the County library in Eureka, and a Black Heritage Ball on Feb 28 at the Wharfinger. Details and more events at <a href="https://www.blackhumboldt.com/black-liberation-month/">blackhumboldt.com/black-liberation-month</a>. At Cal Poly Humboldt, Black feminist and reproductive justice activist Dr. Loretta J. Ross, whose most recent book is <i> Calling in: How to Start Making Change With Those Youd Rather Cancel</i> will speak on Wed. Feb. 11, 5:30-7pm in Student Activities Center 225 - Kate Buchanan Room (KBR). More CPH events at <a href="https://www.humboldt.edu/umoja/events">humboldt.edu/umoja/events</a>.'
                 },
         {
-            "title": "Resistance and Remembrance",
-            "body": 'Satsuki Ima will present on "Resistance and Remembrance" at Cal Poly Humboldts celebration of the Fred Korematsu Day of Civil Liberties and the Constitution. Ina is a licensed psychotherapist specializing in community trauma. Her activism has included cofounding Tsuru for Solidarity, a nonviolent, direct-action project of Japanese American social justice advocates working to end detention sites. Fred Korematsu was a Japanese-American civil rights activist who resisted interment as a  Japanese American during WWII and got his  conviction overturned by the Supreme Court. January 26, 5:00 to 6:30 pm. CPH Native Forum (BSS 162). Free. <a href="https://www.hapihumboldt.org">hapihumboldt.org</a>.'
+            "title": "Resistance continues",
+            "body": 'Speak out against ICE and the many threats to our democracy and our lives in Arcata on Feb. 7, 11 am at the Post Office corner of the Plaza.  And celebrate Presidents Day by speaking out against the dangerous and destructive person currently in that job, Feb. 16 at noon at the Eureka Courthouse.'
         },
         {   
-            "title": "Know Your Rights/Rapid Response",
-         "body": "Centro del Pueblo and the Humboldt Rapid Response Network invite migrant communities and people of all culture, languages, and families to a Know Your Rights info session. You'll learn about your rights, get to know the Rapid Response Network, and share tools to protect ourselves as a community. Saturday Jan. 31, time/location TBD. For details, email  info@cdpueblo.com or call 707-683-5293."
-        },
-        {
-             "title": "Protest Jan. 17",
-             "body": "An Arcata Indivisible group is organizing a Women's March Free America Walkout protest on Jan. 17 connected to protests around the country against all the many ways the current federal administration is hurting people, the planet, and democracy.  At the Eureka Courthouse, Saturday January 17 noon-2 pm. <a href='https://action.womensmarch.com/events/humboldt-county-free-america-rally'> More details here</a>. See you there!"
+            "title": "The Vagina Monologues",
+             "body": 'This years 25th anniversary Humboldt production includes original monlogues written by and for the local community, centering trans, non-binary, gender-nonconforming, and queer voices.  Valentines weekend, Friday Feb. 13, Sat. Feb 14, and Sunday Feb. 15. Doors at 7 Friday/Saturday, 1 pm Sunday. Tickets are $25, at <a href="https://www.bit.ly/2026VM">bit.ly/2026VM</a> or at the door, benefitting Humboldt Women for Shelter and Together Women Rise.  No one turned away for lack of funds.'
         }
     ]
 };
