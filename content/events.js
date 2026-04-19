c1b0d7257b232aa9fb5e9d40ab0b23b58847e7a0
lwghsue <suejh@humboldt1.com>
2026-04-19 14:40:40 -0700

    Remove 'Keep Speaking Out' event and update May Day details

--- content/events.js
+++ content/events.js
@@ -1,16 +1,12 @@
 events = {
     "events": [
-        {
-            "title": "Keep Speaking Out",
-            "body": 'The news about what our government is doing gets more apalling every day. Weekly demonstrations continue at noon on Saturday in Trinidad (410 Main street) and Arcata (on the Plaza), and noon on Sundays in Ferndale (by the gazebo at City Hall). Theres also a May Day event, below. '  
-        },
         {   
             "title": "Womens Environmental Justice Conference",
              "body": '<b>Native Women in Western Systems of Education</b> is a space to listen and learn from Native Women on their experiences in systems of education.There will be keynote presentations, workshops, and panels. At the Native American Forum, CPH 9am - 5pm Satuday April 25. Register <a href="https://docs.google.com/forms/d/e/1FAIpQLSfGiS7DqxCvtJ_k8hJw3zYfIGPgEmvMXdFaelHvMYeyNe6hWQ/viewform">here</a>.'
         },
         {
             "title": "May Day Rally, March, and Movie",
-            "body": 'Friday May 1 is a General Strike. Stand in solidarity with all workers--no work, no school, no online/big box shopping. That evening, join the Humboldt & Del Norte Central Labor Council and Centro del Pueblo in a rally and march.  The rally, with music and speakers, is at the Humboldt County courthouse from 5-6:30 pm.  At 6:35, people will march to the Eureka Theater where Organized Labor Movie Night starts with local organizations tabling, food trucks, and concessions, followed by a 7:30 screening of "Inglorious Basterds". More info at <a href="https://www.mobilize.us/mobilize/event/930219/">mobilize.us</a>.'
+            "body": 'Friday May 1 is a General Strike. Stand in solidarity with all workers--no work, no school, no shopping. That evening, join the Humboldt & Del Norte Central Labor Council and Centro del Pueblo in a rally and march. The rally, with music and speakers, is at the Humboldt County courthouse from 5-6:30 pm.  At 6:35, people will march to the Eureka Theater where Organized Labor Movie Night starts with local organizations tabling, food trucks, and concessions, followed by a 7:30 screening of Inglorious Basterds. More info at <a href="https://www.mobilize.us/mobilize/event/930219/">mobilize.us</a>.'
         },
         {
             "title": "Dancing in the Rain",
