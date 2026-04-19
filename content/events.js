0362306b0a5682c0b9dc1707a48936bccd39e51f
lwghsue <suejh@humboldt1.com>
2026-04-19 11:07:38 -0700

    Add May Day Rally and Movie event to events list
    Added details about the May Day rally and movie event.

--- content/events.js
+++ content/events.js
@@ -2,7 +2,7 @@ events = {
     "events": [
         {
             "title": "Keep Speaking Out",
-            "body": 'The news about what our government is doing gets more apalling every day, and it was wonderful to see so many of you at the big demo in Eureka on March 28. Weekly demonstrations continue at noon on Saturday in Trinidad (410 Main street) and Arcata (on the Plaza), and noon on Sundays in Ferndale (by the gazebo at City Hall). <br /> Friday May 1 is a <b> General Strike </b>. Stand in solidarity with all workers--no work, no school, no online/big box shopping. That evening, join the Humboldt & Del Norte Central Labor Council and Centro del Pueblo in a rally and march.  The rally, with music and speakers, is at the Humboldt County courthouse from 5-6:30 pm.  At 6:35, people will march to the Eureka Theater where Organized Labor Movie Night starts with local organizations tabling, food trucks, and concessions, followed by a 7:30 screening of "Inglorious Basterds". More info at <a href="https://www.mobilize.us/mobilize/event/930219/">mobilize.us</a>.'
+            "body": 'The news about what our government is doing gets more apalling every day, and it was wonderful to see so many of you at the big demo in Eureka on March 28. Weekly demonstrations continue at noon on Saturday in Trinidad (410 Main street) and Arcata (on the Plaza), and noon on Sundays in Ferndale (by the gazebo at City Hall).'<br /> Friday May 1 is a <b> General Strike </b>. Stand in solidarity with all workers--no work, no school, no online/big box shopping. That evening, join the Humboldt & Del Norte Central Labor Council and Centro del Pueblo in a rally and march.  The rally, with music and speakers, is at the Humboldt County courthouse from 5-6:30 pm.  At 6:35, people will march to the Eureka Theater where Organized Labor Movie Night starts with local organizations tabling, food trucks, and concessions, followed by a 7:30 screening of "Inglorious Basterds". More info at <a href="https://www.mobilize.us/mobilize/event/930219/">mobilize.us</a>.'
         },
         {   
             "title": "Womens Environmental Justice Conference",
@@ -11,6 +11,10 @@ events = {
         {
             "title": "Dancing in the Rain",
             "body": 'Support the North Coast Rape Crisis Team at their now-annual Gala on May 2. The theme is Old Hollywood Glam and there will be food, music, dancing, and of course lots of opportunities to support the NCRCT.  Tickets $125 (and up), at <a href="https://ncrct.org/gala">ncrct.org/gala</a>.'
+        },
+        {
+            "title": "May Day Rally, March, and Movie",
+            "body": 'Friday May 1 is a General Strike. Stand in solidarity with all workers--no work, no school, no online/big box shopping. That evening, join the Humboldt & Del Norte Central Labor Council and Centro del Pueblo in a rally and march.  The rally, with music and speakers, is at the Humboldt County courthouse from 5-6:30 pm.  At 6:35, people will march to the Eureka Theater where Organized Labor Movie Night starts with local organizations tabling, food trucks, and concessions, followed by a 7:30 screening of "Inglorious Basterds". More info at <a href="https://www.mobilize.us/mobilize/event/930219/">mobilize.us</a>.'
         },
     ]
 };
