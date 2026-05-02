bff3aefa6feeb28f4b962669fef9cb56dd3d9540
lwghsue <suejh@humboldt1.com>
2026-05-01 22:24:36 -0700

    Update event details and add new events

--- content/events.js
+++ content/events.js
@@ -1,12 +1,20 @@
 events = {
     "events": [
         {
-            "title": "May Day Rally, March, and Movie",
-            "body": 'Friday May 1 is a General Strike. Stand in solidarity with all workers--no work, no school, no shopping. That evening, join the Humboldt & Del Norte Central Labor Council and Centro del Pueblo in a rally and march. The rally, with music and speakers, is at the Humboldt County courthouse from 5-6:30 pm.  At 6:35, people will march to the Eureka Theater where Organized Labor Movie Night starts with local organizations tabling, food trucks, and concessions, followed by a 7:30 screening of Inglorious Basterds. More info at <a href="https://www.mobilize.us/mobilize/event/930219/">mobilize.us</a>.'
+            "title": "Beltane with the Sisters",
+            "body": 'the Eureka Sisters of Perpetual Indulgence invite you to their annual Beltane celebration in Halvorsen Park, Eureka, on Saturday May 2 with a maypole, a ribbon dance, and crowning of a May Queen. Picnic supplies, ribbons, and flowers are welcome. The festival starts at 12:30, and the ritual is 1:30-2. More info at <a href="https://www.eurekasisters.org/">eurekasisters.org</a>.'
         },
         {
             "title": "Dancing in the Rain",
             "body": 'Support the North Coast Rape Crisis Team at their now-annual Gala on May 2. The theme is Old Hollywood Glam and there will be food, music, dancing, and of course lots of opportunities to support the NCRCT.  Tickets $125 (and up), at <a href="https://ncrct.org/gala">ncrct.org/gala</a>.'
-        },        
+        }, 
+        {
+            "title": "Atalanta's Victory Run",
+            "body": 'Runners, walkers, strollers, and participants of all abilities are welcome to the annual walk/run benefitting the Breast & Gyn Health Project. All are welcome to participate in the race, but only those identifying as women will be eligible for awards. The run start at 10 am May 10th. Early-bird pricing $18 for 2-mile course, $23 for 5-mile course. Both courses start and finish at the Arcata co-op, rain or shine. <a href="https://atalanta-run.com">atalanta-run.com</a>'
+        },
+        {
+            "title": "Pride Season Begins",
+            "body": 'Planning is underway for Pride celebrations around Humboldt and Del Norte counties, with two set for this month. <b> Sunny Brea Middle School </b> will celebrate Pride Night on May 21, 5:30-7:30 pm. All local LGBTQ students, staff, families and allies welcome. Details <a href="https://arcataschooldistrict.org/apps/news/article/2070831>here</a>. And Two Feathers Native American Family Services <b>Rainbow Extravaganze 6.0 </b> is on May 30, 11-2, 1560 Betty Ct. in McKinleyville.  Food, prizes, joy, and community!'
+        },
     ]
 };
