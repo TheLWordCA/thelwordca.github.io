e4005bc3af88709ed7ea078e0ee936ddb7358f6b
lwghsue <suejh@humboldt1.com>
2026-05-03 21:47:17 -0700

    Update events list with new entries
    Removed outdated events and added new event details.

--- content/events.js
+++ content/events.js
@@ -1,20 +1,16 @@
 events = {
     "events": [
-        {
-            "title": "Beltane with the Sisters",
-            "body": 'the Eureka Sisters of Perpetual Indulgence invite you to their annual Beltane celebration in Halvorsen Park, Eureka, on Saturday May 2 with a maypole, a ribbon dance, and crowning of a May Queen. Picnic supplies, ribbons, and flowers are welcome. The festival starts at 12:30, and the ritual is 1:30-2. More info at <a href="https://www.eurekasisters.org/">eurekasisters.org</a>.'
-        },
-        {
-            "title": "Dancing in the Rain",
-            "body": 'Support the North Coast Rape Crisis Team at their now-annual Gala on May 2. The theme is Old Hollywood Glam and there will be food, music, dancing, and of course lots of opportunities to support the NCRCT.  Tickets $125 (and up), at <a href="https://ncrct.org/gala">ncrct.org/gala</a>.'
-        }, 
-        {
+       {
             "title": "Atalanta's Victory Run",
             "body": 'Runners, walkers, strollers, and participants of all abilities are welcome to the annual walk/run benefitting the Breast & Gyn Health Project. All are welcome to participate in the race, but only those identifying as women will be eligible for awards. The run start at 10 am May 10th. Early-bird pricing $18 for 2-mile course, $23 for 5-mile course. Both courses start and finish at the Arcata co-op, rain or shine. <a href="https://atalanta-run.com">atalanta-run.com</a>'
         },
         {
+            "title": "Women's Herbal Symposium",
+             "body": 'The Northern California Womens Herbal Symposium is 3 weekends a year, two in the spring and one in the fall, of women sharing space and herbal and sustainability knowledge. At at Black Oak Ranch near Laytonville. This years gatherings are May 15-18, May 22-25 (waitlisted), and September 4-7.  Details, history, and registration at <a href="https://www.womensherbalsymposium.org">womensherbalsymposium.org</a>'
+        },
+        {
             "title": "Pride Season Begins",
-            "body": 'Planning is underway for Pride celebrations around Humboldt and Del Norte counties, with two set for this month. <b> Sunny Brea Middle School </b> will celebrate Pride Night on May 21, 5:30-7:30 pm. All local LGBTQ students, staff, families and allies welcome. Details <a href="https://arcataschooldistrict.org/apps/news/article/2070831>here</a>. And Two Feathers Native American Family Services <b>Rainbow Extravaganze 6.0 </b> is on May 30, 11-2, 1560 Betty Ct. in McKinleyville.  Food, prizes, joy, and community!'
+            "body": 'Planning is underway for Pride celebrations around Humboldt and Del Norte counties, with two set for this month. <b> Sunny Brea Middle School </b> will celebrate Pride Night on May 21, 5:30-7:30 pm. All local LGBTQ students, staff, families and allies welcome. Details <a href="https://arcataschooldistrict.org/apps/news/article/2070831">here</a>. And Two Feathers Native American Family Services <b>Rainbow Extravaganze 6.0 </b> is on May 30, 11-2, 1560 Betty Ct. in McKinleyville.  Food, prizes, joy, and community!'
         },
     ]
 };
