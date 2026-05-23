b4e8be6aed498144d4fa037269de5759f0261406
lwghsue <suejh@humboldt1.com>
2026-05-22 21:18:58 -0700

    Update event details and add test event

--- content/events.js
+++ content/events.js
@@ -1,8 +1,12 @@
 events = {
     "events": [
         {
-            "title": "Pride Season",
-            "body": 'Pride Season starts early and continues through June and beyond in Humboldt & Del Norte. At the end of May, catch Two Feathers Native American Family Services <b>Rainbow Extravaganze 6.0 </b> on May 30, 11-2, 1560 Betty Ct. in McKinleyville.  Food, prizes, joy, and community!.  The <b>Eureka Sisters</b> will kick off June celebratons at the Friday Night Market on June 5 at the Gazebo in Old Town Eureka from 5:30-8:30 pm. Music, glitter, and cupcakes!  Saturday the 6th is a <b>Community Picnic </b> at Da Yas Park, 2625 Summer Street in Eureka, noon-3pm. Organized by folks from the L-Word, Queer Humboldt, and Redwood Pride, its a low-key space to connect with community. Bring a picnic and/or food to share, or get food from Pupuseria San Miguel (limited vouchers available).  The <b>Ferndale Pride March </b> is on June 13:gather at 10 at city hall, 834 Main Street, March at 11, and then celebrate with music, vendors, etc from noon-5 at Firemans Park. 
+            "title": "test",
+            "body": "hi"
         },
+        {
+            "title": "hello",
+            "body": "world"
+        }
     ]
 };
