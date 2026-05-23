7fec0de15d46668a3c5a32d3f868e93561453ddc
lwghsue <suejh@humboldt1.com>
2026-05-22 20:31:48 -0700

    Refactor 'Pride Picnic' event description
    Updated the body of the 'Pride Picnic' event to consolidate information into a single paragraph.

--- content/events.js
+++ content/events.js
@@ -2,20 +2,8 @@ events = {
     "events": [
         {
             "title": "Pride Picnic",
-                "body": "Celebrate Pride Month at an all-ages community picnic on Saturday June 6 from noon-3pm at Eureka's Da' Yas park (2625 Summer Street, eurekaca.gov/636/Da-Yas-Park). Bring a picnic, and food to share if you want, or get food from Pupuseria San Miguel. Vouchers for pupusas available in limited quantities.
-We'll be in the group picnic area, next to the giant egrets. There's space to spread out on the lawn behind us. The park is accessible and has bathrooms, courts, and wonderful play equipment.
-A low-key space to connect and celebrate with the larger queer community. Organized by a small group of  folks from the L-word, Queer Humboldt, and Redwood Pride with financial assistance from Queer Humboldt.
-If you want to help with the event (setup, cleanup, or just general assistance) email Julie: julie@queerhumbolt.org."
-                    },
-        
-        {
-            "title": "Atalanta's Victory Run",
-            "body": 'Runners, walkers, strollers, and participants of all abilities are welcome to the annual walk/run benefitting the Breast & Gyn Health Project. All are welcome to participate in the race, but only those identifying as women will be eligible for awards. The run start at 10 am May 10th. Early-bird pricing $18 for 2-mile course, $23 for 5-mile course. Both courses start and finish at the Arcata co-op, rain or shine. <a href="https://www.atalanta-run.com">atalanta-run.com</a>'
-        },
-        {
-            "title": "Women's Herbal Symposium",
-             "body": 'The Northern California Womens Herbal Symposium is 3 weekends a year, two in the spring and one in the fall, of women sharing space and herbal and sustainability knowledge. At at Black Oak Ranch near Laytonville. This years gatherings are May 15-18, May 22-25 (waitlisted), and September 4-7.  Details, history, and registration at <a href="https://www.womensherbalsymposium.org">womensherbalsymposium.org</a>'
-        },
+                "body": "Celebrate Pride Month at an all-ages community picnic on Saturday June 6 from noon-3pm at Eureka's Da' Yas park (2625 Summer Street, eurekaca.gov/636/Da-Yas-Park). Bring a picnic, and food to share if you want, or get food from Pupuseria San Miguel. Vouchers for pupusas available in limited quantities. We'll be in the group picnic area, next to the giant egrets. There's space to spread out on the lawn behind us. The park is accessible and has bathrooms, courts, and wonderful play equipment. A low-key space to connect and celebrate with the larger queer community. Organized by a small group of  folks from the L-word, Queer Humboldt, and Redwood Pride with financial assistance from Queer Humboldt. If you want to help with the event (setup, cleanup, or just general assistance) email Julie: julie@queerhumbolt.org."
+         },        
         {
             "title": "Pride Season Begins",
             "body": 'Planning is underway for Pride celebrations around Humboldt and Del Norte counties, with two set for this month. <b> Sunny Brea Middle School </b> will celebrate Pride Night on May 21, 5:30-7:30 pm. All local LGBTQ students, staff, families and allies welcome. Details <a href="https://arcataschooldistrict.org/apps/news/article/2070831">here</a>. And Two Feathers Native American Family Services <b>Rainbow Extravaganze 6.0 </b> is on May 30, 11-2, 1560 Betty Ct. in McKinleyville.  Food, prizes, joy, and community!'
