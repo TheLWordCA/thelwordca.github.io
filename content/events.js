849001daa51c6b9b90f5bcaf8c39fbf31c18c97c
lwghsue <suejh@humboldt1.com>
2026-05-22 21:12:30 -0700

    Modify events for Pride Season and remove picnic
    Updated event details for Pride Season and removed the Pride Picnic entry.

--- content/events.js
+++ content/events.js
@@ -1,12 +1,8 @@
 events = {
     "events": [
         {
-            "title": "Pride Picnic",
-                "body": 'Celebrate Pride Month at an all-ages community picnic on Saturday June 6 from noon-3pm at Da Yas park, 2625 Summer Streetin Eureka. Bring a picnic, and food to share if you want, or get food from Pupuseria San Miguel. Vouchers for pupusas available in limited quantities. Well be in the group picnic area, next to the giant egrets. Theres space to spread out on the lawn behind us. The park is accessible and has bathrooms, courts, and wonderful play equipment. A low-key space to connect and celebrate with the larger queer community. Organized by a small group of folks from the L-word, Queer Humboldt, and Redwood Pride with financial assistance from Queer Humboldt. If you want to help with the event (setup, cleanup, or just general assistance) email Julie: julie@queerhumbolt.org.'
-         },        
-        {
             "title": "Pride Season",
-            "body": 'Pride Season starts early and continues through June and beyond in Humboldt & Del Norte. At the end of May, catch Two Feathers Native American Family Services <b>Rainbow Extravaganze 6.0 </b> on May 30, 11-2, 1560 Betty Ct. in McKinleyville.  Food, prizes, joy, and community!. Next up will be the <b>Eureka Sisters</b> at the Friday Night Market, at the Gazebo in Old Town Eureka from 5:30-8:30 pm on Friday June 5. Music, glitter, and cupcakes!  Saturday the 6th is the community Picnic at Da Yas Park in Eureka (above), 
+            "body": 'Pride Season starts early and continues through June and beyond in Humboldt & Del Norte. At the end of May, catch Two Feathers Native American Family Services <b>Rainbow Extravaganze 6.0 </b> on May 30, 11-2, 1560 Betty Ct. in McKinleyville.  Food, prizes, joy, and community!.  The <b>Eureka Sisters</b> will kick off June celebratons at the Friday Night Market on June 5 at the Gazebo in Old Town Eureka from 5:30-8:30 pm. Music, glitter, and cupcakes!  Saturday the 6th is a <b>Community Picnic </b> at Da Yas Park, 2625 Summer Street in Eureka, noon-3pm. Organized by folks from the L-Word, Queer Humboldt, and Redwood Pride, its a low-key space to connect with community. Bring a picnic and/or food to share, or get food from Pupuseria San Miguel (limited vouchers available).  The <b>Ferndale Pride March </b> is on June 13:gather at 10 at city hall, 834 Main Street, March at 11, and then celebrate with music, vendors, etc from noon-5 at Firemans Park. 
         },
     ]
 };
