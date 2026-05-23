dc87bac11a3573f48f7b0c63fbef8c4c38369bc3
lwghsue <suejh@humboldt1.com>
2026-05-22 20:47:08 -0700

    Update event details for Pride events

--- content/events.js
+++ content/events.js
@@ -2,11 +2,11 @@ events = {
     "events": [
         {
             "title": "Pride Picnic",
-                "body": "Celebrate Pride Month at an all-ages community picnic on Saturday June 6 from noon-3pm at Eureka's Da' Yas park (2625 Summer Street, eurekaca.gov/636/Da-Yas-Park). Bring a picnic, and food to share if you want, or get food from Pupuseria San Miguel. Vouchers for pupusas available in limited quantities. We'll be in the group picnic area, next to the giant egrets. There's space to spread out on the lawn behind us. The park is accessible and has bathrooms, courts, and wonderful play equipment. A low-key space to connect and celebrate with the larger queer community. Organized by a small group of  folks from the L-word, Queer Humboldt, and Redwood Pride with financial assistance from Queer Humboldt. If you want to help with the event (setup, cleanup, or just general assistance) email Julie: julie@queerhumbolt.org."
+                "body": "Celebrate Pride Month at an all-ages community picnic on Saturday June 6 from noon-3pm at Eureka's Da' Yas park (2625 Summer Street). Bring a picnic, and food to share if you want, or get food from Pupuseria San Miguel. Vouchers for pupusas available in limited quantities. We'll be in the group picnic area, next to the giant egrets. There's space to spread out on the lawn behind us. The park is accessible and has bathrooms, courts, and wonderful play equipment. A low-key space to connect and celebrate with the larger queer community. Organized by a small group of folks from the L-word, Queer Humboldt, and Redwood Pride with financial assistance from Queer Humboldt. If you want to help with the event (setup, cleanup, or just general assistance) email Julie: julie@queerhumbolt.org."
          },        
         {
-            "title": "Pride Season Begins",
-            "body": 'Planning is underway for Pride celebrations around Humboldt and Del Norte counties, with two set for this month. <b> Sunny Brea Middle School </b> will celebrate Pride Night on May 21, 5:30-7:30 pm. All local LGBTQ students, staff, families and allies welcome. Details <a href="https://arcataschooldistrict.org/apps/news/article/2070831">here</a>. And Two Feathers Native American Family Services <b>Rainbow Extravaganze 6.0 </b> is on May 30, 11-2, 1560 Betty Ct. in McKinleyville.  Food, prizes, joy, and community!'
+            "title": "Pride Season",
+            "body": 'Pride Season starts early and continues through June and beyond in Humboldt & Del Norte. At the end of May, catch Two Feathers Native American Family Services <b>Rainbow Extravaganze 6.0 </b> on May 30, 11-2, 1560 Betty Ct. in McKinleyville.  Food, prizes, joy, and community!. Next up will be the <b>Eureka Sisters</b> at the Friday Night Market, at the Gazebo in Old Town Eureka from 5:30-8:30 pm on Friday June 5. Music, glitter, and cupcakes!  Saturday the 6th is the community Picnic at Da Yas Park in Eureka (above), 
         },
     ]
 };
