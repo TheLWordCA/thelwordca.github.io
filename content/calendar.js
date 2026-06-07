074800311058de2bf13db23f0a2f9a363387d422
lwghsue <suejh@humboldt1.com>
2026-06-06 19:00:07 -0700

    Fix calendar event formatting for June 3

--- content/calendar.js
+++ content/calendar.js
@@ -1,5 +1,5 @@
 calendar = [
 'June 2 9 16 23 30 Teen Creative Art Group 5:30-7pm. vail@rhizomewellness.org',
-'June 3 10 17 24 "WTF is the World" 2-3pm. vail@rhizomewellness.org',
+'June 3 10 17 24 "WTF is the World", 2-3pm. vail@rhizomewellness.org',
 'June 27 Lesbian brunch 11am Stacy Sue 440-897-5864',
 ];
