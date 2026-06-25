8c43340c175862b8cf0f1da97119c7813a521afa
lwghsue <suejh@humboldt1.com>
2026-06-25 13:15:30 -0700

    Replace Juneteenth event with All of Us 250 event

--- content/events.js
+++ content/events.js
@@ -5,8 +5,8 @@ events = {
              "body": 'June has been busy with celebrations so far, and you can still catch a few more. Del Norte and Tribal Lands <b> DNATL Pride</b> includes an art show at the Crescent Art Gallery all month, a bowling party at Tsunami Lanes 4 to 7 pm on Friday June 26 and a dog show, parade and festival, 10 am - 2 pm and entertainment 6-9 pm Saturday the 27th at the Crescent City UMC church, 664 H Street.<a href="https://www.DNLGBTQ.org/dnatlpride2026">DNLGBTQ.org/dnatlpride2026</a>. Also on June 27th, join the United Congregational Christian Church in Eureka for their 3rd annual <b> Community Pride Dinner </b>. A free spaghetti dinner for LGBTQ+ people, allies, families, and friends, 5:30-7:30 pm at the UCCchurch, 900 Hodgson St., Eureka 707 445-5488 for info. '
         },
         {
-            "title": "Juneteenth",
-            "body": 'Humboldt Black & Brown folks invite you to join in celebrating freedom from June 18 through 21. The <b> Juneteeth festival </b> is Saturday June 20 from 12 to 7pm at Halvorsen Park. We will be tabling there, if you want to help, email lword@sonic.net. And on Sunday the 21st the <b>Black Brunch and Ball</b> features ballroom culture, Queer Black performance, brunch and community at Synapsis in Eureka 11 am-1 pm.  More events and details at <a href="https://www.blackhumboldt.com/juneteenth-celebration">blackhumbolt.com</a>.'
+            "title": "All of Us 250 June 27",
+            "body": 'Make your voice heard about what you want your country to be like at 250 years!  All of us 250 is a nationwide mobilization for Trusth, Equality, and Freedom. Join us at the Eureka Courthouse from 12-2pm on Saturday June 27.  Food Not Bombs will be holding a food drive; all groups welcome to table. Email 50501humboldt@gmail.com with questions or concerns.'
         }
     ]
 };
