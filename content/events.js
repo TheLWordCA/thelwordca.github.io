39e3dbebf13328e145d0e98c1f0ebefd8d500931
lwghsue <suejh@humboldt1.com>
2026-07-02 22:05:11 -0700

    Update event titles and descriptions in events.js

--- content/events.js
+++ content/events.js
@@ -1,16 +1,16 @@
 events = {
     "events": [
         {
-             "title": "Pride Season",
-             "body": 'June has been busy with celebrations so far, and you can still catch a few more. Del Norte and Tribal Lands <b> DNATL Pride</b> includes an art show at the Crescent Art Gallery all month, a bowling party at Tsunami Lanes 4 to 7 pm on Friday June 26 and a dog show, parade and festival, 10 am - 2 pm and entertainment 6-9 pm Saturday the 27th at the Crescent City UMC church, 664 H Street.<a href="https://www.DNLGBTQ.org/dnatlpride2026">DNLGBTQ.org/dnatlpride2026</a>. Also on June 27th, join the United Congregational Christian Church in Eureka for their 3rd annual <b> Community Pride Dinner </b>. A free spaghetti dinner for LGBTQ+ people, allies, families, and friends, 5:30-7:30 pm at the UCCchurch, 900 Hodgson St., Eureka 707 445-5488 for info. '
+             "title": "Queer CaberGay",
+             "body": 'Redwood Curtain is extending Pride Month into July!  Join the talented performers of Redwood Curtain and feel the beauty of the love and fun that the queerness of musical theater has to offer.  This will be a pair of performances that will make you smile and laugh and want to come back and see it again. July 10 & 11, 8 pm, Fifth & D St. theater, $20 <a href="https://www.redwoodcurtain.com/">redwoodcurtain.com</a>'
         },
         {
-            "title": "All of Us 250 June 27",
-            "body": 'Make your voice heard about what you want your country to be like at 250 years!  All of us 250 is a nationwide mobilization for Truth, Equality, and Freedom. Join us at the Eureka Courthouse from 12-2pm on Saturday June 27.  Food Not Bombs will be holding a food drive; all groups welcome to table. Email 50501humboldt@gmail.com with questions or concerns.'
+            "title": "Celebrate the 4th",
+            "body": 'Join us on Saturday July 4th noon-2pm at the courthouse.  The theme is "FREE AMERICA FOR ALL OF US" We have the numbers, we are on the path to victory...POWER TO THE PEOPLE!!  Please spread the word!!!'
         },
         {
             "title": "LGBTQ Art Show Humboldt 2026",
-            "body": "Submissions are open for this year's show. Art will be displayed August 1-30. Find out more and submit an application at lgbtqartshowhumboldtca.webador.com"
+            "body": 'Submissions are open for this years show. Art will be displayed August 1-30. Find out more and submit an application at <a href="https://lgbtqartshowhumboldtca.webador.com/">lgbtqartshowhumboldtca</a>'
         }
     ]
 };
