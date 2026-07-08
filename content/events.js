161ada31bec7758836213c3ea86796b67d06ab82
lwghsue <suejh@humboldt1.com>
2026-07-07 21:55:27 -0700

    Replace 'Celebrate the 4th' event with 'Disability Pride'

--- content/events.js
+++ content/events.js
@@ -5,8 +5,8 @@ events = {
              "body": 'Redwood Curtain is extending Pride Month into July!  Join the talented performers of Redwood Curtain and feel the beauty of the love and fun that the queerness of musical theater has to offer.  This will be a pair of performances that will make you smile and laugh and want to come back and see it again. July 10 & 11, 8 pm, Fifth & D St. theater, $20 <a href="https://www.redwoodcurtain.com/">redwoodcurtain.com</a>'
         },
         {
-            "title": "Celebrate the 4th",
-            "body": 'Join us on Saturday July 4th noon-2pm at the courthouse.  The theme is "FREE AMERICA FOR ALL OF US" We have the numbers, we are on the path to victory...POWER TO THE PEOPLE!!  Please spread the word!!!'
+            "title": "Disability Pride",
+            "body": 'Join Humboldters of all ages and abilities in our 5th annual <b> Disability Pride Month </b> celebration. The Festival at the Adroni will include information, indoor and outdoor play spaces, art, an open mic, food, and a Parade! The parade starts at 3 pm from the Adorni and goes to the boardwalk and back. The Festival goes from 11am to 4pm on Saturday July 25 at the Adorni.center in Eureka. Because we all belong. <a href="https://linktr.ee/disabilitypride/">linktr.ee/disabilitypride</a>'
         },
         {
             "title": "LGBTQ Art Show Humboldt 2026",
