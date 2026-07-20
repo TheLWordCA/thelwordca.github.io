23bf8449e5d35bf1cdb2bfeea71fb4021454870f
lwghsue <suejh@humboldt1.com>
2026-07-19 22:30:36 -0700

    Update event details in events.js

--- content/events.js
+++ content/events.js
@@ -1,8 +1,8 @@
 events = {
     "events": [
         {
-             "title": "Resist ICE and support Voting Rights",
-             "body": '50501 Humboldt is calling for folks to be at the courthouse from 12-2 on Saturday July 18 to protest two recent killings by ICE agenst, in Texas and Maine--and also the recent detention of a Humboldt man in Wyoming.  <br /> And Indivisible Arcata is calling on us to "Make Good Trouble" in honor of John L. Lewis, againts everything the current federal regime is doing to make it harder for people to vote.  Also 12-2 Saturday July 20, Arcata Plaza, southwest corner.'
+             "title": "Support Your Community with Centro del Pueblo",
+             "body": 'Centro del Pueblo and the UC Davis Labor and Community Center are presenting a series of trainings on immigration July 23-25 at the Centro Community Center, 3008 Broadway suite A in Eureka.  <b> Protecting Your Community</b>, on Thursday July 23 at 5:30, covers Your Rights as an observer, Supporting your immigrant neighbors, and upholding rights for all workers. <b>State of Immigration</b>, on Friday July 24 at 5:30, covers Changes in policy, processing times, and California protections for immigrant workers.  And <b>Planning for the Future</b> on Saturday July 25 starting at 10 am, covers Family Protection Planning, Relocation, and Worker Protections. There will also be opportunites to talk with an immigration lawyer.  Email info@centrodelpueblo.com or text 707 683 5293 for info.'
         },
         {
             "title": "Disability Pride",
