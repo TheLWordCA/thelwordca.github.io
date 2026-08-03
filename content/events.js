5ba09c6dffa439ea8a57f4dde76299023cc40094
lwghsue <suejh@humboldt1.com>
2026-08-02 22:09:50 -0700

    Add new event 'Wowzer' to events list

--- content/events.js
+++ content/events.js
@@ -10,11 +10,15 @@ events = {
         },
         {
             "title": "Back to Fool Bingo",
-            "body": 'Join the Eureka Sisters of Perpetual Indulgence in the first Bingo of the season,<b> Back to Fool </b>, at the Eureka Womens club. Doors at 5, game at 6, tickets $25 adv, $35 door.  <a href="https://www.eurekasisters.org">eurekasister.org</a>.'
+            "body": 'Join the Eureka Sisters of Perpetual Indulgence in the first Bingo of the season,<b> Back to Fool </b>, at the Eureka Womens club. Doors at 5, game at 6, tickets $25 adv, $35 door.  <a href="https://www.eurekasisters.org">eurekasisters.org</a>.'
         },
         {
             "title": "Humboldt Obon",
             "body": 'Obon is a Japanese Buddhist custom to honor the spirits of ones ancestors. We celebrate our Obon Festival with a Buddhist blessing, Obon dancing, Humboldt Taiko performances, our ancestors tree, kids games, Asian food vendors, artists, and more. Sunday, August 16, 3-7 pm Creamery District, Arcata <a href="https://www.Hapihumboldt.org/">HapiHumboldt.org</a>.'
+        },
+        {
+            "title": '"Wowzer" in Brookings',
+            '"Wowzer" is a new film about our friend Joanie Lindenmeyer and her 3 books, Nun Better: An Amazing Love Story, Joyously Free: Stories & Tips for LGBTQ+ People, Parents and Allies and Healing Religious Hurts: Stories & Tips to Find Love and Peace. It premiers at the Wild River Film Festival in Brookings, OR August 14-16, 4 pm Fri./Sat, 6 Sunday.  Details soon at <a href="https://www.wildriversfilmfestival.com/festival-events">wildriversfilmfestival.com</a>.'
         }
-    ]
+   ]
 };
