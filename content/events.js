b371daf2c721f3335d78eb27fdc74cdfb2a46c3a
lwghsue <suejh@humboldt1.com>
2026-08-02 21:37:59 -0700

    Update events list with new entries and details

--- content/events.js
+++ content/events.js
@@ -1,16 +1,20 @@
 events = {
     "events": [
         {
-             "title": "Support Your Community with Centro del Pueblo",
-             "body": 'Centro del Pueblo and the UC Davis Labor and Community Center are presenting a series of trainings on immigration July 23-25 at the Centro Community Center, 3008 Broadway suite A in Eureka.  <b> Protecting Your Community</b>, on Thursday July 23 at 5:30, covers Your Rights as an Observer, Supporting your Immigrant Neighbors, and upholding rights for all workers. <b>State of Immigration</b>, on Friday July 24 at 5:30, covers Changes in policy, Processing times, and California protections for immigrant workers.  And <b>Planning for the Future</b> on Saturday July 25 starting at 10 am, covers Family Protection Planning, Relocation, and Worker Protections. There will also be opportunites to talk with an immigration lawyer.  Email info@centrodelpueblo.com or text 707 683 5293 for info.'
-        },
-        {
-            "title": "Disability Pride",
-            "body": 'Join Humboldters of all ages and abilities in our 5th annual <b> Disability Pride Month </b> celebration. The Festival at the Adroni will include information, indoor and outdoor play spaces, art, an open mic, food, and a Parade! The parade starts at 3 pm from the Adorni and goes to the boardwalk and back. The Festival goes from 11am to 4pm on Saturday July 25 at the Adorni.center in Eureka. Because we all belong. <a href="https://linktr.ee/disabilitypride/">linktr.ee/disabilitypride</a>'
-        },
-        {
             "title": "LGBTQ Art Show Humboldt 2026",
-            "body": 'Submissions are open for this years show. Art will be displayed August 1-30. Find out more and submit an application at <a href="https://lgbtqartshowhumboldtca.webador.com/">lgbtqartshowhumboldtca</a>'
+            "body": 'A Variety of Visual Art will be on display at the Two Street Art Lab, 527 2nd Eureka through the month of August, Mon-Fri 10am-3pm..<a href="https://lgbtqartshowhumboldtca.webador.com/">lgbtqartshowhumboldtca</a>'
+        },
+        {
+             "title": "Arcata Lantern Floating Ceremony",
+             "body": 'Arcatas annual ceremony for peace offers space to honor and celebrate departed loved ones and peace, honor those suffering from genocide and opression, and remember the atomic bombings in Hiroshima and Nagasaki and say "Never Again".  Lantern making 9am-1:30pm, Arcata Plaza; program 7:30pm and lantern floating at sunset, Klopp lake, Saturday August 8. Park at Samoa & i street & walk or shuttle to the lake.'
+        },
+        {
+            "title": "Back to Fool Bingo",
+            "body": 'Join the Eureka Sisters of Perpetual Indulgence in the first Bingo of the season,<b> Back to Fool </b>, at the Eureka Womens club. Doors at 5, game at 6, tickets $25 adv, $35 door.  <a href="https://www.eurekasisters.org">eurekasister.org</a>.'
+        },
+        {
+            "title": "Humboldt Obon",
+            "body": 'Obon is a Japanese Buddhist custom to honor the spirits of ones ancestors. We celebrate our Obon Festival with a Buddhist blessing, Obon dancing, Humboldt Taiko performances, our ancestors tree, kids games, Asian food vendors, artists, and more. Sunday, August 16, 3-7 pm Creamery District, Arcata <a href="https://www.Hapihumboldt.org/">HapiHumboldt.org</a>.'
         }
     ]
 };
