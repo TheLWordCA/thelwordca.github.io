902c724a3124fac04147611fb5a172e397651fbf
lwghsue <suejh@humboldt1.com>
2026-08-20 21:59:29 -0700

    Modify events for vigil and bingo season
    Updated event details and added new events.

--- content/events.js
+++ content/events.js
@@ -1,24 +1,13 @@
 events = {
     "events": [
         {
-            "title": "Vigil for lives lost to ICE",
-            "body": "Centro del Pueblo is hosting a vigil for people who have died in ICE enforcement or ICE dentention on Saturday August 8, 11 am, at the Sanctuary Garden in Arcata. Bring flowers, a candle, or just yourself. Stay after for Know Your Rights training."
+            "title": "Vigil for lives lost to ICE and Sanctuary Day Celebration",
+            "body": "Centro del Pueblo is hosting biweekly vigils for people who have died in ICE enforcement or ICE dentention. The next visgil will be on Saturday August 22 at 11 am, at the Sanctuary Garden in Arcata. Bring flowers, a candle, or just yourself. Stay after forKnow your Rights/ Rapid Response Network training.<br /> And join Centro del Pueblo to Celebrate Sanctuary at the I Street Block Party, Monday September 7, noon-6 pm, I street between 6th & 7th, Arcata. Music, dance, food, art, and fun.  Benefit for Centro del Pueblo. "
         },
         {
-             "title": "Arcata Lantern Floating Ceremony",
-             "body": 'Arcatas annual ceremony for peace offers space to honor and celebrate departed loved ones and peace, honor those suffering from genocide and opression, and remember the atomic bombings in Hiroshima and Nagasaki and say "Never Again".  Lantern making 9am-1:30pm, Arcata Plaza; program 7:30pm and lantern floating at sunset, Klopp lake, Saturday August 8. Park at Samoa & i street & walk or shuttle to the lake.'
+             "title": "Bingo Season continues",
+            "body": 'Join the Eureka Sisters of Perpetual Indulgence in another fun evening of fundraising, this time for the North Coast Rape Crisis team. <b> Full Moon Fantasy</b> is on Saturday September 12 at the Eureka Womens club. Doors at 5, game at 6, tickets $25 adv, $35 door.  <a href="https://www.eurekasisters.org">eurekasisters.org</a>.'
         },
-        {
-            "title": "Back to Fool Bingo",
-            "body": 'Join the Eureka Sisters of Perpetual Indulgence in the first Bingo of the season,<b> Back to Fool </b>, at the Eureka Womens club. Doors at 5, game at 6, tickets $25 adv, $35 door.  <a href="https://www.eurekasisters.org">eurekasisters.org</a>.'
-        },
-        {
-            "title": "Humboldt Obon",
-            "body": 'Obon is a Japanese Buddhist custom to honor the spirits of ones ancestors. We celebrate our Obon Festival with a Buddhist blessing, Obon dancing, Humboldt Taiko performances, our ancestors tree, kids games, Asian food vendors, artists, and more. Sunday, August 16, 3-7 pm Creamery District, Arcata <a href="https://www.Hapihumboldt.org/">HapiHumboldt.org</a>.'
-        },
-        {
-            "title": "<i>Wowzer</i> in Brookings",
-            "body": '<i>Wowzer</i> is a new film about our friend Joanie Lindenmeyer and her 3 books, Nun Better: An Amazing Love Story, Joyously Free: Stories & Tips for LGBTQ+ People, Parents and Allies and Healing Religious Hurts: Stories & Tips to Find Love and Peace. It premiers at the Wild River Film Festival in Brookings, OR August 14-16, 4 pm Fri./Sat, 6 Sunday. Details soon at <a href="https://www.wildriversfilmfestival.com/festival-events">wildriversfilmfestival.com</a>.'
         }
    ]
 };
