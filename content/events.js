4d783237147a67b024e372e3f29e39b6f0cc7914
lwghsue <suejh@humboldt1.com>
2026-09-11 11:53:58 -0700

    Add new events to the events list

--- content/events.js
+++ content/events.js
@@ -1,12 +1,15 @@
 events = {
     "events": [
         {
-            "title": "Vigil for lives lost to ICE and Sanctuary Day Celebration",
-            "body": 'Centro del Pueblo is hosting biweekly <b>vigils for people who have died in ICE enforcement or ICE dentention</b>. The next vigil will be on Saturday September 5 at 11 am, at the Sanctuary Garden in Arcata. Bring flowers, a candle, or just yourself. Stay after for Know your Rights and Rapid Response Network training. <br /> And on Labor Day, Sept. 7, join Centro del Pueblo to Celebrate Sanctuary at the <b>I Street Block Party,</b> noon-6 pm on I street between 6th & 7th, Arcata. Music, dance, food, art, and fun.  Benefits Centro del Pueblo.'
-        },
-        {
              "title": "Bingo Season continues",
             "body": 'Join the Eureka Sisters of Perpetual Indulgence in another fun evening of fundraising, this time for the North Coast Rape Crisis team. <b> Full Moon Fantasy</b> is on Saturday September 12 at the Eureka Womens club. Doors at 5, game at 6, tickets $25 adv, $35 door.  <a href="https://www.eurekasisters.org">eurekasisters.org</a>.'
-        }
+        },
+        {
+             "title": "Queer Folk Duo in Arcata",
+             "body": 'Irish singer-songwriter Grainne Hunt and San Diego drummer and vocalist Jules Stewart will be at the Arcata Playhouse this month. Their rich harmonies, heartfelt stories, and magnetic onstage chemistry remind audiences of the Indigo Girls or Brandi Carlisle. Saturday September 29, 7:30 pm Arcata Playhouse, $20.50. <a href="https://www.playhousearts.org">playhousearts.org</a>'
+        },
+              "title": "Resist & Fight Back",  
+            "body": 'This November is a critical opportunity for all of us to be part of stopping some of the damage being done to us, our friends and allies, our communities, people around the world and our planet by the current U.S. government. Make sure you are registered, vote, donate or work for candidates who speak for sanity, and encourage others to do the same.  Make connections at local weekly demos-- Saturday in Eureka (courthouse), Arcata (on the Plaza), and Trinidad (410 Main street) and Sundays, noon, in Ferndale (by the gazebo at City Hall). Women in Black are at the courthouse Tuesdays at noon and Ferndale City Hall Fridays at 5pm. We hear that theres a nationwide demo planned for October 17. See you out there.' 
+     }
    ]
 };
